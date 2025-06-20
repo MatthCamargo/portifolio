738eae344ac721c44aa4fefa6c19015e3043a3c4
Matheus <maatheuscamargo1@gmail.com>
2025-06-20 17:54:13 -0300

    fix: atualizao

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <div className='hero'>
         <img src={profile_img} alt="" />
-        <h1> Eu sou Matheus Camargo, desenvolvedor front end no Brasil</h1>
-        <p>descrição</p>
+        <h1> <span>Eu sou Matheus Camargo,</span>  desenvolvedor front end no Brasil</h1>
+        <p>Desenvolvedor Front-end Júnior responsável pela implementação de interfaces web responsivas, utilizando HTML, CSS, JavaScript e frameworks como React. Atua em correções de bugs e pequenas melhorias guiado por times de design e desenvolvimento.</p>
         <div className="hero-action">
           <div className="hero-connect">Connect with me</div>
           <div className="hero-resume">My resume</div>
